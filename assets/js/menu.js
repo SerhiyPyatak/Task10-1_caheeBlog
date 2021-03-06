f193e0913f1a222b677e73bd48ec70103a61443d
SerhiyPyatak <serhiypyatak@yahoo.com>
2021-03-06 09:57:04 +0200

    This commit adds responsibility to menu bar

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,9 +1,9 @@
 jQuery(document).ready(function() {
 	jQuery('.menu-bar__toggle').click(function(e) {
-		jQuery(this).toggleClass('active');
-		jQuery('.menu-bar__menu_hero').toggleClass('active');
+        jQuery(this).toggleClass('active');
+        jQuery('.menu-bar__menu_hero').toggleClass('active');
         jQuery('.menu-bar__menuitem_hero').toggleClass('active');
-
+    
         if (jQuery(this).hasClass('active')) {
             $('.toggle__icon').removeClass('fa-bars');
             $('.toggle__icon').addClass('fa-times');
@@ -12,7 +12,15 @@ jQuery(document).ready(function() {
             $('.toggle__icon').removeClass('fa-times');
             $('.toggle__icon').addClass('fa-bars');
         }
+    
+        e.preventDefault();
+    });
 
-		e.preventDefault();
-	});
+    jQuery('.menu-bar__menuitem_hero').click(function(e) {
+        jQuery(this).removeClass('active');
+        jQuery('.menu-bar__menu_hero').removeClass('active');
+        jQuery('.menu-bar__toggle').removeClass('active');
+        $('.toggle__icon').removeClass('fa-times');
+        $('.toggle__icon').addClass('fa-bars');
+    });
 });
